a17ac881df1141afe36bcf22b5c40ed00682643e
jack-the-drought <mouathsai@yahoo.fr>
2017-10-07 21:50:39 +0100

    reset tests

--- webapp/test.py
+++ webapp/test.py
@@ -93,7 +93,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(
             self.fetcher.fetch_available_types_and_statuses(),
             {
-                'statuses': ['offline', 'online', 'booha'],
+                'statuses': ['offline', 'online'],
                 'types': ['gateway', 'sensor']
             }
         )
